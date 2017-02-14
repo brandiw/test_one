921d739c5aff36cf861a4640add3a4e48aae5011
brandiw <brandimichelle.0911@gmail.com>
2017-02-14 11:35:02 -0800

    testing

---
+++ main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $("#doit").click(function(){
+    console.log("Doing");
+    $.ajax({
+      method: "GET",
+      url: "https://www.opentdb.com/api.php?amount=10&category=9&type=multiple&token=436ca162dc13768dae3dfb22610c9944301293c966ee578b3635467c541e08c1"
+    }).done(function(data){
+      console.log("done");
+      console.log(data);
+    });
+  });
+});
